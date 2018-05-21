c03e65feecc347e1ef1bf5c4f6f5c3184cc08745
Tom Morrell <tmorrell@caltech.edu>
2018-05-21 09:19:33 -0700

    Update metadata

--- update_record.py
+++ update_record.py
@@ -1,11 +1,12 @@
 from caltechdata_api import caltechdata_edit 
 import os
 
-dates = ['1914', '1933', '1935-1939', '1944-1945', '1948', '1954-1955','1957-1961', '1963', '1965-1976', '1978-1980']
+dates = ['1914', '1933', '1935/1939', '1944/1945', '1948',
+        '1954/1955','1957/1961', '1963', '1965/1976', '1978/1980']
 locations = ['Mountain Creek','Niobrara River','American Canal','Colorado River','Hii River','Middle Loup River',
         'Rio Grande River','Canal','Atchafalaya River','Mississippi River','Red River','Rio Grande near Bernalillo, NM',
         'River, Portugal Rivers','Chop Canals','N Saskatchewan River and Elbow River',
-        'South American River and Canal / Rio Magdelena and Canal de Dique','Oak Creek, Oregon','Trinity River.',
+        'South American River and Canal / Rio Magdelena and Canal de Dique','Oak Creek, Oregon','Trinity River',
         'Rio Grande Conveyance Channel','Snake and Clearwater River','Missouri River','ACOP Canal']
 
 idv = 943
