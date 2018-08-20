085f2667a461fd8bdc6fb73721b35ff0cb898051
Tom Morrell <tmorrell@caltech.edu>
2018-08-20 15:59:43 -0700

    Working update for thesis key file generation

--- update_thesis_file.py
+++ update_thesis_file.py
@@ -1,23 +1,29 @@
-import os,subprocess,json
+import os,subprocess,json,csv
 
 record_list = {}
 count = 0
 url = 'https://thesis.library.caltech.edu/rest/eprint/'
-keys = subprocess.check_output(["eputil",'-json',url])
+keys =\
+subprocess.check_output(["eputil",'-json',url],universal_newlines=True)
 #keys = keys.split('/n')
 username = os.environ['EPUSER']
 password = os.environ['EPPASSWD']
 url =\
 'https://'+username+':'+password+'@thesis.library.caltech.edu/rest/eprint/'
-for k in keys:
+print(keys)
+lines = keys.splitlines()
+for l in lines:
+    line = l.strip()#Clean up white space
+    line = line.strip(',[]')#And symbols
+    if line != '':
         count = count + 1
         if count % 100 == 0:
             print(count)
         metadata =\
-        subprocess.check_output(["eputil",'-json',url+str(k)+'.xml'],universal_newlines=True)
+        subprocess.check_output(["eputil",'-json',url+line+'.xml'],universal_newlines=True)
         metadata = json.loads(metadata)
         if metadata != {}:
-            record_list[k]=metadata['eprint'][0]['official_url']
+            record_list[int(line)]=metadata['eprint'][0]['official_url']
         else:
             print("Bad Record: "+k)
             print(metadata)
