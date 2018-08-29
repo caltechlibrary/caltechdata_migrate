5f5e914631e73ae2087802ddd4a3b392fbbc0e2b
Tom Morrell <tmorrell@caltech.edu>
2018-08-29 13:59:00 -0700

    Complete harvest script

--- harvest_geo.py
+++ harvest_geo.py
@@ -59,9 +59,33 @@ for h in hits['hits']['hits']:
     for s in metadata['subjects']:
         subjects = subjects + s['subject'] + ','
     subjects = subjects[0:-1]
+    dates = ''
+    for d in metadata['dates']:
+        if d['dateType'] == 'Collected':
+            dates = dates + d['date'] +','
+    dates = dates[0:-1]
+    geo = ''
+    if 'geoLocations' in metadata:
+        loc = metadata['geoLocations']
+        for g in loc:
+            if 'geoLocationBox' in g:
+                box = g['geoLocationBox']
+                geo = geo + 'Geographic Location Bounding Box: '\
+                + str(box['eastBoundLongitude']) + ' Degrees East; '\
+                + str(box['westBoundLongitude']) + ' Degrees West; '\
+                + str(box['northBoundLatitude']) + ' Degrees North; '\
+                + str(box['southBoundLatitude']) + ' Degrees South\n'
+            if 'geoLocationPoint' in g:
+                box = g['geoLocationPoint']
+                geo = geo + 'Geographic Location Point: '\
+                + str(box['pointLongitude']) + ' Degrees Longitude; '\
+                + str(box['pointLatitude']) + ' Degrees Latitude\n'
+            if 'geoLocationPlace' in g:
+                geo = geo + 'Geographic Location Place: '+g['geoLocationPlace']
 
-    record = {'doi':doi,'item_title':item_title,
-            'item_number':item_number,'subjects':subjects[0:-1]}
+    record = {'doi':doi,'item_title':item_title,'title':title,
+            'item_number':item_number,'subjects':subjects,
+            'dates':dates,'geo':geo}
         
     result = resolver in records
     if result == False:
@@ -89,4 +113,35 @@ for h in hits['hits']['hits']:
         if error == False:
             existing.append(record)
             records[resolver] = existing
+
+for resolver in records:
+    record = records[resolver]
+    output = {'resolver':resolver}
+    output_str = 'Supplemental Files Information:\n'
+    for r in record:
+        output['identifier_'+r['item_number']] = r['doi']
+        start_str = 'Supplement '+r['item_number']+' in CaltechDATA: '
+        output['description_'+r['item_number']] = start_str + r['item_title'] 
+        output_str = output_str + r['title']+'\n'
+        if r['dates'] != '':
+            output_str = output_str + 'Date(s) Collected: ' +r['dates']
+        if r['geo'] != '':
+            output_str = output_str + geo
+        output['subjects'] = r['subjects']
+    output['additional'] = output_str
+    dataset.create(collection,resolver,output)
+
+#Google sheet export
+output_sheet = '1vhw4cz2txRlY5iWRhQveOs4nk-4aW6FMZKbDOX6cgu0'
+sheet_name = "Sheet1"
+sheet_range = "A1:CZ"
+client_secret = '/etc/client_secret.json'
+export_list = ['.resolver','.subjects','.additional']
+for j in range(1,21):
+    k = str(j)
+    export_list.append('.identifier_'+k)
+    export_list.append('.description_'+k)
+dataset.use_strict_dotpath(False)
+response = dataset.export_gsheet(collection,client_secret,output_sheet,sheet_name,sheet_range,'true',dot_exprs=export_list)
+print("Export: ",response)
 
