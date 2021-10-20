839feab326eb0b412d740db64e96d7ad0048c295
Tom Morrell <tmorrell@caltech.edu>
2021-10-20 12:06:28 -0700

    Update check metadata

--- check_metadata.py
+++ check_metadata.py
@@ -1,28 +1,30 @@
-import os,json,csv,argparse
-import requests
-from datacite import DataCiteMDSClient, schema40
-from decustomize_schema import decustomize_schema
+import os, json, csv, argparse
+from datacite import schema43
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description=\
-    "get_metadata queries the caltechDATA (Invenio 3) API\
-    and returns DataCite-compatable metadata")
-    parser.add_argument('filen', metavar='file', type=str, nargs='+',\
-            help='File name for each record of interest')
+    parser = argparse.ArgumentParser(
+        description="get_metadata queries the caltechDATA (Invenio 3) API\
+    and returns DataCite-compatable metadata"
+    )
+    parser.add_argument(
+        "filen",
+        metavar="file",
+        type=str,
+        nargs="+",
+        help="File name for each record of interest",
+    )
 
     args = parser.parse_args()
 
     for n in args.filen:
 
-        infile = open(n,'r')
+        infile = open(n, "r")
         metadata = json.load(infile)
 
-        #assert schema40.validate(metadata)
-        #Debugging if this fails
-        v = schema40.validator.validate(metadata)
-        errors = sorted(v.iter_errors(instance), key=lambda e:e.path)
+        # assert schema43.validate(metadata)
+        # Debugging if this fails
+        v = schema43.validator.validate(metadata)
+        errors = sorted(v.iter_errors(instance), key=lambda e: e.path)
         for error in errors:
-                print(error.message)
-
-
+            print(error.message)
